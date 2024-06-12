30837d1f33818d085fc8bdac87435982c8207a02
aruj-micro1 <aruj@micro1.ai>
2024-06-12 17:26:27 +0530

    chat history

--- app.py
+++ app.py
@@ -14,24 +14,18 @@ VALID_API_KEYS = {os.getenv("FLASK_API_KEY")}
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
-def chat_scale_ai(query):
+def chat_scale_ai(query,history):
     prompt_template = """
         Act as an expert lawyer and use the following pieces of context of cases to formulate an answer based on them. If you don't know the answer, just say that you don't know, don't try to make up an answer.
         {context}
         Question: {question}
         Helpful Answer:"""
+   
     try:
-        with open('chat_history.json', 'r') as chat_hist_load:
-            chat_hist_dict = json.load(chat_hist_load)
-            chat_hist_dict_for_llm = [(x, y) for x, y in chat_hist_dict['Question']]
-        if len(chat_hist_dict) > 2:
-            chat_hist_dict = {}
-            with open('chat_history.json', 'w') as chat_hist_load:
-                json.dump({}, chat_hist_load)
-    except FileNotFoundError:
-        chat_hist_dict = {}
-    except json.JSONDecodeError:
-        chat_hist_dict = {}
+        chat_hist_dict_for_llm = [(x,y) for x,y in zip(history['User'],history['Assistant'])] 
+    except:
+        chat_hist_dict_for_llm = []
+    
 
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     loaded_db = Chroma(persist_directory='./chromadb', embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"))
@@ -45,18 +39,10 @@ def chat_scale_ai(query):
         question_generator=question_generator,
     )
 
-    if len(chat_hist_dict) > 0:
+    if len(chat_hist_dict_for_llm) > 0:
         payload = qa({"question": str(query), "chat_history": chat_hist_dict_for_llm})
     else:
         payload = qa({"question": str(query), "chat_history": []})
-
-    if 'Question' not in chat_hist_dict:
-        chat_hist_dict["Question"] = []
-
-    chat_hist_dict['Question'].append((query, payload['answer']))
-
-    with open('chat_history.json', 'w') as chat_hist:
-        json.dump(chat_hist_dict, chat_hist)
 
     return payload['answer']
 
@@ -67,10 +53,12 @@ def ask():
         return jsonify({"error": "Invalid API key"}), 401
     data = request.json
     query = data.get('query')
+    history = data.get('history')
     if not query:
         return jsonify({"error": "Query parameter is required"}), 400
-
-    answer = chat_scale_ai(query)
+    if not history:
+        history = []
+    answer = chat_scale_ai(query,history)
     return jsonify({"answer": answer})
 
 if __name__ == '__main__':
